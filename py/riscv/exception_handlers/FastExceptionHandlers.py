5b4f7a7a6529d610ff607fa2f728ebcfeaebd5c6
mbrother <mbrother@futurewei.com>
2020-06-24 11:43:46 -0700

    Fast handlers code changes needing debug

---
+++ py/riscv/exception_handlers/FastExceptionHandlers.py
@@ -0,0 +1,46 @@
+from riscv.exception_handlers.FastExceptionHandlersBase import FastExceptionHandlersBaseRISCV
+
+class FastEmptyHandlerRISCV(FastExceptionHandlersBaseRISCV):
+
+    def processException(self):
+        self.debug('[FastEmptyHandlerRISCV] process exception %s, privilege level: %s' % (self.mErrCode, self.mPrivLevel))
+
+        print('[FastEmptyHandlerRISCV] process exception %s, privilege level: %s' % (self.mErrCode, self.mPrivLevel))
+
+
+
+class FastSkipInstructionHandlerRISCV(FastExceptionHandlersBaseRISCV):
+
+    def processException(self):
+        self.debug('[FastSkipInstructionHandlerRISCV] process exception %s, privilege level: %s' % (self.mErrCode, self.mPrivLevel))
+
+        print('[FastSkipInstructionHandlerRISCV] process exception %s, privilege level: %s' % (self.mErrCode, self.mPrivLevel))
+
+
+        scratch_reg_index = self.mScratchRegisters[0]
+
+        epc_name = 'mepc' if self.mPrivLevel == 'M' else 'sepc' if self.mPrivLevel == 'S' else 'unknown_pc_error' 
+
+        self.mAssemblyHelper.genReadSystemRegister(scratch_reg_index, epc_name)
+        self.mAssemblyHelper.genAddImmediate(scratch_reg_index, 4)
+        self.mAssemblyHelper.genWriteSystemRegister(epc_name, scratch_reg_index)
+
+
+
+
+class FastRecoveryAddressHandlerRISCV(FastExceptionHandlersBaseRISCV):
+    
+    def processException(self):
+        self.debug('[FastRecoveryAddressHandlerRISCV] process exception %s, privilege level: %s' % (self.mErrCode, self.mPrivLevel))
+
+        scratch_reg_index = self.mScratchRegisters[0]
+        recovery_reg_index = self.mScratchRegisters[0]
+
+        # get the next address from the address table
+        self.mAddrTable.getAddress(recovery_reg_index, scratch_reg_index)
+
+        # update the appropriate epc with the recovery address
+        epc_name = 'mepc' if self.mPrivLevel == 'M' else 'sepc' if self.mPrivLevel == 'S' else 'unknown_pc_error' 
+
+        self.mAssemblyHelper.genWriteSystemRegister(epc_name, recovery_reg_index)
+
---
+++ py/riscv/exception_handlers/FastExceptionHandlersBase.py
@@ -0,0 +1,65 @@
+from riscv.PrivilegeLevel import PrivilegeLevelRISCV
+from base.exception_handlers.ReusableSequence import ReusableSequence
+from riscv.exception_handlers.ExceptionHandlerContext import RegisterCallRole
+
+# Base class for Force 'Fast' mode synchronous exception handlers
+class FastExceptionHandlersBaseRISCV(ReusableSequence):
+
+    def __init__(self, aGenThread, aFactory, aStack):
+        super().__init__(aGenThread, aFactory, aStack)
+
+        self.mErrCode = None
+        self.mScratchRegisters = None
+        self.mPrivLevel = None
+        self.mAddrTable = None
+        self.use_addr_table = False
+
+    def generateHandlerS(self, **kwargs):
+        self.genHandler(**kwargs)
+
+    def generateHandlerM(self, **kwargs):
+        self.genHandler(**kwargs)
+
+    def generateHandler(self, **kwargs):
+        self.genHandler(**kwargs)
+
+    # main entry point - generates exception handler. The handler must be able to generate/handle
+    # exception at any privilege mode
+    def genHandler(self, **kwargs):
+        self.debug('[FastExceptionHandlersBaseRISCV] genHandler')
+
+        print('[FastExceptionHandlersBaseRISCV] genHandler')
+
+        try:
+            handler_context = kwargs['handler_context']
+        except KeyError:
+            self.error('INTERNAL ERROR: one or more arguments to fast mode exception handler genHandler method missing.')
+
+        self.mErrCode = handler_context.mErrCode
+        self.mScratchRegisters = handler_context.getScratchRegisterIndices(RegisterCallRole.ALL, 2)
+        self.mPrivLevel = handler_context.mPrivLevel
+        self.mAddrTable = handler_context.mAddrTable
+
+        if self.mPrivLevel not in ('S', 'M'):
+            self.error('INTERNAL ERROR: unknown privilege level: "%s%.' % self.mPrivLevel)
+
+        # call method to generate the code to process the exception
+        self.processException()
+
+        # return from exception
+        self.notice("BLAH" +self.mPrivLevel  )
+        priv_level = PrivilegeLevelRISCV[self.mPrivLevel]
+
+        self.mAssemblyHelper.genExceptionReturn(priv_level)
+
+
+    def useAddressTable(self):
+        self.use_addr_table = True
+        info_set = {}
+        info_set['Function'] = 'AddrTableEC'
+        info_set['EC'] = self.err_code
+        self.exceptionRequest('UpdateHandlerInfo', info_set)
+
+    # generate the code to process the exception
+    def processException(self):
+        NotImplementedError
