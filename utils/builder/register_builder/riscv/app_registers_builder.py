2c974c32cf5f8b9d8eec48bb2c5bdb7e2e097f8f
Ahmed Ramzy <ahmedgalalramzy@gmail.com>
2020-10-06 11:20:11 +0800

    added support to build 32-bit app registers
    Signed-off-by: Ahmed Ramzy <ahmedgalalramzy@gmail.com>

--- utils/builder/register_builder/riscv/app_registers_builder.py
+++ utils/builder/register_builder/riscv/app_registers_builder.py
@@ -34,23 +34,40 @@ def create_app_register(aName, aSize, aIndex, aType, aClass, aBoot, aAltPhysReg=
     new_reg.addRegisterFieldInOrder(new_field)
     return new_reg
 
-def create_quad_precision_app_register(aIndex):
+def create_double_precision_app_register(aIndex, aRegSize):
+    
+    d_reg = Register(**{'name':"D%d" % aIndex, 'length':64, 'index':aIndex, 'type':"FPR", 'class':None, 'boot':0x3000})
+    numberOfReg = 64 // aRegSize
+    for i in range(0, numberOfReg):
+        d_field = RegisterField("D%d_%d" % (aIndex, i))
+        d_field.mPhysicalRegister = "f%d_%d" % (aIndex, i)
+        d_bfield = BitField(aRegSize, 0)
+        d_field.mBitFields.append(d_bfield)
+        d_reg.addRegisterFieldInOrder(d_field)
+    
+    return d_reg
+
+    
+
+def create_quad_precision_app_register(aIndex, aRegSize):
     q_reg = Register(**{'name':"Q%d" % aIndex, 'length':128, 'index':aIndex, 'type':"FPR", 'class':"LargeRegister", 'boot':0})
-    for i in range(0, 2):
+    numberOfReg = 128 // aRegSize
+    for i in range(0, numberOfReg):
         q_field = RegisterField("Q%d_%d" % (aIndex, i))
         q_field.mPhysicalRegister = "f%d_%d" % (aIndex, i)
-        q_bfield = BitField(64, 0)
+        q_bfield = BitField(aRegSize, 0)
         q_field.mBitFields.append(q_bfield)
         q_reg.addRegisterFieldInOrder(q_field)
 
     return q_reg
 
-def create_vector_app_register(aIndex):
+def create_vector_app_register(aIndex, aRegSize):
     v_reg = Register(**{'name':"V%d" % aIndex, 'length':128, 'index':aIndex, 'type':"VECREG", 'class':"LargeRegister", 'boot':0x3000})
-    for i in range(0, 2):
+    numberOfReg = 128 // aRegSize
+    for i in range(0, numberOfReg):
         v_field = RegisterField("V%d_%d" % (aIndex, i))
         v_field.mPhysicalRegister = "v%d_%d" % (aIndex, i)
-        v_bfield = BitField(64, 0)
+        v_bfield = BitField(aRegSize, 0)
         v_field.mBitFields.append(v_bfield)
         v_reg.addRegisterFieldInOrder(v_field)
 
@@ -58,41 +75,48 @@ def create_vector_app_register(aIndex):
 
 def build_app_registers():
 
-    app_register_doc = RegistersDocument("RISC-V Registers")
+    for size in (32, 64):
+        app_register_doc = RegistersDocument("RISC-V %s-bit Registers" % size)
 
-    # Add x1-x31 registers
-    for i in range(1, 32):
-        app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("x%d" % i, 64, i, "GPR"))
-        app_register_doc.addRegister(create_app_register("x%d" % i, 64, i, "GPR", None, 0x1000))
+        # Add x1-x31 registers
+        for i in range(1, 32):
+            app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("x%d" % i, size, i, "GPR"))
+            app_register_doc.addRegister(create_app_register("x%d" % i, size, i, "GPR", None, 0x1000))
 
-    # Add zero register
-    app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("zero", 64, 0, "GPR", "PhysicalRegisterRazwi", 0))
-    app_register_doc.addRegister(create_app_register("x0", 64, 0, "ZR", "ReadOnlyZeroRegister", 0, "zero"))
+        # Add zero register
+        app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("x0", size, 0, "GPR", "PhysicalRegisterRazwi", 0))
+        app_register_doc.addRegister(create_app_register("x0", size, 0, "ZR", "ReadOnlyZeroRegister", 0, "x0"))
 
-    # Add PC register
-    app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("PC", 64, 32, "PC"))
-    app_register_doc.addRegister(create_app_register("PC", 64, 32, "PC", None, 0))
+        # Add PC register
+        app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("PC", size, 32, "PC"))
+        app_register_doc.addRegister(create_app_register("PC", size, 32, "PC", None, 0))
 
-    # Add f0-f31 and S0-S31
-    for i in range(0, 32):
-        app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("f%d_0" % i, 64, i, "FPR", aSubIndex=0))
-        app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("f%d_1" % i, 64, i, "FPR", aSubIndex=1))
-        app_register_doc.addRegister(create_app_register("S%d" % i, 32, i, "FPR", None, 0, "f%d_0" % i))
+        # number of sub-registers needed for quad and vector 128-bit registers
+        numberOfSubReg = 128 // size
 
-    # Add D0-D31
-    for i in range(0, 32):
-        app_register_doc.addRegister(create_app_register("D%d" % i, 64, i, "FPR", None, 0x3000, "f%d_0" % i))
-    # Add Q0-Q31
-    for i in range(0, 32):
-        app_register_doc.addRegister(create_quad_precision_app_register(i))
+        # Add f0-f31 and S0-S31
+        for i in range(0, 32):
+            for subIndex in range(numberOfSubReg):
+                app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("f%d_%d" % (i, subIndex), size, i, "FPR", aSubIndex=subIndex))
+            
+            app_register_doc.addRegister(create_app_register("S%d" % i, 32, i, "FPR", None, 0, "f%d_0" % i))
 
-    # add vector registers V0-V31
-    for i in range(0, 32):
-        app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("v%d_0" % i, 64, i, "VECREG", aSubIndex=0))
-        app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("v%d_1" % i, 64, i, "VECREG", aSubIndex=1))
-        app_register_doc.addRegister(create_vector_app_register(i))
+            # Add D0-D31
+        for i in range(0, 32):
+            app_register_doc.addRegister(create_double_precision_app_register(i, size))
 
-    app_register_doc.writeXmlFile('output/app_registers.xml')
+        # Add Q0-Q31
+        for i in range(0, 32):
+            app_register_doc.addRegister(create_quad_precision_app_register(i, size))
+
+            # add vector registers V0-V31
+        for i in range(0, 32):
+            for subIndex in range(numberOfSubReg):
+                app_register_doc.addPhysicalRegister(PhysicalRegister.createPhysicalRegister("v%d_%d" % (i, subIndex), size, i, "VECREG", aSubIndex=subIndex))
+            
+            app_register_doc.addRegister(create_vector_app_register(i, size))
+        
+        app_register_doc.writeXmlFile('output/app_registers_rv%s.xml' % size)
 
 if __name__ == "__main__":
     build_app_registers()
