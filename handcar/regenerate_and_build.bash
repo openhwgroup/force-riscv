4718a03c01186daae0eba89e239d5987743bd709
Jim Thornsberry <jthornsb@futurewei.com>
2020-11-04 11:48:33 -0800

    Improved the handcar regenerate_and_build.bash script
    Signed-off-by: Jim Thornsberry <jthornsb@futurewei.com>

--- handcar/regenerate_and_build.bash
+++ handcar/regenerate_and_build.bash
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 #
 # Copyright (C) [2020] Futurewei Technologies, Inc.
 #
@@ -15,18 +14,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+PAUSE='read -sn1 -p'
+
+while getopts "y" opt; do
+    case "$opt" in
+    y)  PAUSE='read -t5 -sn1 -p'
+        ;;
+    esac
+done
 
 make clean
+rm -rf standalone
 git clone https://github.com/riscv/riscv-isa-sim standalone
 cd standalone
 git checkout 61f0dab33f7e529cc709908840311a8a7dcb23ce
+
+echo "Preparing to remove DTC dependencies"
+
+sed -i  '/^if test x"$DTC" == xno; then :/,/fi/ s/^/# /'  ./configure
+cat ./configure | grep -B10 -A7 'if test x"$DTC" == xno; then :'
+$PAUSE "Please review edit(s) then press Enter to continue"; echo
+
+sed -i  '/^  if (dtb_pid == 0)/,/^  }/ s/^/\/\/ /'             ./riscv/dts.cc
+sed -i  '/^  waitpid(dts_pid, &status, 0);/,/^  }/ s/^/\/\/ /' ./riscv/dts.cc
+sed -i  '/^  waitpid(dtb_pid, &status, 0);/,/^  }/ s/^/\/\/ /' ./riscv/dts.cc
+cat ./riscv/dts.cc | grep -B9 -A10 -e"if (dtb_pid == 0) {" -e"waitpid(dts_pid, &status, 0);" -e"waitpid(dtb_pid, &status, 0);"
+$PAUSE "Please review edit(s) then press Enter to continue"; echo
+
 ./configure
-make
+make -j
 cd ..
+
+# NOTE: so_build/cosim/inc contains the handcar_cosim_wrapper.h file.  Don't remove it.
+rm -rf src spike_mod so_build/cosim/src bin
+
 mkdir src
-mkdir spike_mod
-mkdir spike_mod/insns
-mkdir so_build/cosim/src
+mkdir -p spike_mod/insns
+mkdir -p so_build/cosim/src
 mkdir bin
 ./create_handcar_files.bash  
 ./filesurgeon.py
