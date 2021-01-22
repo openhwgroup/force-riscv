45cc9ff4b8deda39a89dce9dd90d9b19603c207a
Noah Sherrill <nsherril@futurewei.com>
2021-01-22 12:02:35 -0800

    Add missing license headers
    Signed-off-by: Noah Sherrill <nsherril@futurewei.com>

--- tests/riscv/_v_fctrl.py
+++
@@ -1,3 +0,0 @@
-control_items = [ #{"fname":"instructions/v_instructions/_def_fctrl.py", "generator" : {"--cfg" : "config/riscv.config", }},
-                  {"fname":"instructions/v_instructions/_noiss_fctrl.py", "generator" : {"--cfg" : "config/riscv.config", }},
-                ]
--- utils/builder/instruction_builder/riscv/input/split.py
+++ utils/builder/instruction_builder/riscv/input/split.py
@@ -4,16 +4,19 @@ xml_header = '<?xml version="1.0" encoding="UTF-8"?>'
 license_string = '''
 <!--
  Copyright (C) [2020] Futurewei Technologies, Inc.
+
  FORCE-RISCV is licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
+
   http://www.apache.org/licenses/LICENSE-2.0
+
  THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
  FIT FOR A PARTICULAR PURPOSE.
  See the License for the specific language governing permissions and
  limitations under the License.
--->
+ -->
 '''
 
 startRootTag = "<instruction_file>\n"
