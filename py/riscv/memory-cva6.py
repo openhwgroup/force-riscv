87b0dceca6ddae4d0650d4e703e98d52cea41cf9
quswar-abid <quswar.abid@10xengineers.ai>
2022-08-11 17:06:17 +0500

    edited memory file

--- py/riscv/memory-cva6.py
+++ py/riscv/memory-cva6.py
@@ -21,4 +21,4 @@ class FpixReporter(object):
 
 
 def configure_memory(interface):
-    interface.addMemoryRange(0, 0x000080000000, 0x0000bfffffff)
+    interface.addMemoryRange(0, 0x000080000000, 0x0000BFFFFFFF)
